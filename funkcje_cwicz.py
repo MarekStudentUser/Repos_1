cc696500a4ec689cedabcb296a6a96fe21ca4c4c
MarekStudentUser <104886996+MarekStudentUser@users.noreply.github.com>
2022-05-04 08:22:33 +0200

    Code1

---
+++ funkcje_cwicz.py
@@ -0,0 +1,35 @@
+def dlugosc(x):
+    licz = 0
+    for znak in x:
+        licz += 1
+    return licz
+
+def minimum(x):
+    min = x[0]
+    for el in x:
+        if el<min:
+            min = el
+    return el
+
+def alternatywa(x,y):
+    if x == True or y == True:
+        return 1
+    else:
+        return 0
+
+def pierwiastkuj(x):
+    a = x
+    while a - (x / a)) > d
+        if a*a>x:
+            a = a - a/2
+        else:
+            a = a + a/2
+        print(a)
+    return a
+
+# slowo = [2,5,8,4,9,2,4,1,1]
+# print(dlugosc(slowo))
+# print(minimum(slowo))
+#
+# print(alternatywa(2,1))
+print(pierwiastkuj(9))
